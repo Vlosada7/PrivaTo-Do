4e40a8e81e17e38b884fe21315d9980290258bae
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-04-07 17:25:28 +0200

    Feat: Save the Blockchain with the new data on a file

--- controller/Blocks.js
+++ controller/Blocks.js
@@ -12,7 +12,7 @@ const createBlock = async (req, res) => {
 					hash: crypto.SHA256(req.body + this.timeStamp).toString(),
 				});
 				await newBlock.save();
-				res.status(201).send(newBlock);
+				res.status(201).send({ message: "New block created", block: newBlock });
 			} else {
 				const newBlock = new Block({
 					timeStamp: Date.now(),
@@ -23,7 +23,7 @@ const createBlock = async (req, res) => {
 					previous: lastBlock.hash,
 				});
 				await newBlock.save();
-				res.status(201).send(newBlock);
+				res.status(201).send({ message: "New block created", block: newBlock });
 			}
 		} catch (error) {
 			console.error(error);
@@ -35,7 +35,40 @@ const createBlock = async (req, res) => {
 };
 
 const completeBlock = async (req, res) => {
-	res.send("Task completed");
+	const blockHash = req.body.hash;
+	if (blockHash) {
+		try {
+			const selectedBlock = await Block.findOne({
+				hash: blockHash,
+			});
+			if (selectedBlock) {
+				const lastBlock = await Block.findOne().sort({ timeStamp: -1 });
+				const newBlock = new Block({
+					timeStamp: Date.now(),
+					data: selectedBlock.data,
+					hash: crypto
+						.SHA256(selectedBlock.data + this.timeStamp + lastBlock.hash)
+						.toString(),
+					previous: lastBlock.hash,
+					completed: true,
+				});
+				await newBlock.save();
+				res
+					.status(201)
+					.send({
+						message: "Block created with the alteration",
+						block: newBlock,
+					});
+			} else {
+				res.status(404).send({ message: "Block not found" });
+			}
+		} catch (error) {
+			console.error(error);
+			res.status(500).send({ message: "API error" });
+		}
+	} else {
+		res.status(400).send({ message: "Hash is missing" });
+	}
 };
 
 module.exports = { createBlock, completeBlock };
